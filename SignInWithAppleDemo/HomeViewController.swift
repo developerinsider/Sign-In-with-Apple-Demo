ce6609d8726f72014342034fe57c52625524332f
Vineet Choudhary <vineetchoudhary@live.in>
2019-06-05 16:11:56 +0530

    Initial UI Setup

---
+++ SignInWithAppleDemo/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  SignInWithAppleDemo
+//
+//  Created by Vineet Choudhary on 05/06/19.
+//  Copyright © 2019 Developer Insider. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
